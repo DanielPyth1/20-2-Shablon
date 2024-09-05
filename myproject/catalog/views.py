14bf7cd726a28b06c71944d0da9cd45501da2744
Daniel <danieelpark@gmail.com>
2024-09-05 14:37:35 +0400

    выполнение задания

---
+++ myproject/catalog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'catalog/product_detail.html', context)
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {'object_list': products}
+    return render(request, 'catalog/product_list.html', context)
