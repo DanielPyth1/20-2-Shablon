91746570cdbcf311c621396754b8dc7f18cf5d14
Daniel <danieelpark@gmail.com>
2024-09-05 16:29:33 +0400

    изменение и добавление

--- myproject/catalog/admin.py
+++ myproject/catalog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+
+# admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'image')
+    search_fields = ('name', 'description')
--- myproject/catalog/models.py
+++ myproject/catalog/models.py
@@ -4,7 +4,12 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    manufactured_at = models.DateField()
+    image = models.ImageField(upload_to='products/', blank=True, null=True)  # Добавляем поле для изображения
+    #manufactured_at = models.DateField()
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
