7cc667555aa262d1eca1b401772625cd8744ba45
Daniel <danieelpark@gmail.com>
2024-09-05 21:05:08 +0400

    Исправление после сдачи

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'catalog/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,6 +65,8 @@ TEMPLATES = [
         },
     },
 ]
+
+
 
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
