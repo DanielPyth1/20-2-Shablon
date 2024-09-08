ee2d9c555ffbac2c963563ef3692e73e83bc554c
Daniel <danieelpark@gmail.com>
2024-09-08 13:42:28 +0400

    Исправление после сдачи

--- myproject/catalog/views.py
+++ myproject/catalog/views.py
@@ -4,9 +4,9 @@ from .models import Product
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {'product': product}
-    return render(request, 'includes/product_detail.html', context)
+    return render(request, 'catalog/product_detail.html', context)
 
 def product_list(request):
     products = Product.objects.all()
     context = {'object_list': products}
-    return render(request, 'includes/product_list.html', context)
+    return render(request, 'catalog/product_list.html', context)
