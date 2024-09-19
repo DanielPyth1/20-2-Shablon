d3ad9be7ebf490be95d96f9b865a537f2c9927b3
Daniel <danieelpark@gmail.com>
2024-09-19 19:55:01 +0400

    Задание 3 , 21.1 FBV и CBV

--- myproject/catalog/views.py
+++ myproject/catalog/views.py
@@ -3,6 +3,7 @@ from django.views.generic.list import ListView
 from .models import Product, BlogPost
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.utils.text import slugify
 
 
 class ProductDetailView(DetailView):
@@ -21,10 +22,19 @@ class BlogPostListView(ListView):
     model = BlogPost
     template_name = 'catalog/blog_post_list.html'
 
+    def get_queryset(self):
+        return BlogPost.objects.filter(is_published=True)
+
 
 class BlogPostDetailView(DetailView):
     model = BlogPost
     template_name = 'catalog/blog_post_detail.html'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset=queryset)
+        obj.view_count += 1
+        obj.save()
+        return obj
 
 
 class BlogPostCreateView(CreateView):
@@ -32,11 +42,20 @@ class BlogPostCreateView(CreateView):
     template_name = 'catalog/blog_post_form.html'
     fields = ['title', 'slug', 'content', 'preview', 'is_published']
 
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.slug = slugify(instance.title)
+        instance.save()
+        return super().form_valid(form)
+
 
 class BlogPostUpdateView(UpdateView):
     model = BlogPost
     template_name = 'catalog/blog_post_form.html'
     fields = ['title', 'slug', 'content', 'preview', 'is_published']
+
+    def get_success_url(self):
+        return reverse_lazy('blog_post_detail', args=[self.object.id])
 
 
 class BlogPostDeleteView(DeleteView):
